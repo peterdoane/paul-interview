467bf8da5604c3737c6add9160f3f83cbfbcbcde
Paul Pham <ppham@local-box.org>
2015-03-21 05:09:55 -0700

    Solve HTTP collect puzzle.

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,11 +1,18 @@
 var http = require('http');
+var bl = require('bl');
 
 var url = process.argv[2];
 
 http.get(url, function(response) {
 	response.setEncoding('utf8');
-	response.on("data", function(data) {
-		console.log(data);
-	})
-});
+	response.pipe(bl(function(err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data.length);
+			console.log(data.toString());
+		}
+		}));
+	});
+
 
